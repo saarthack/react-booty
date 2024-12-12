de09de418ebdf06af55f285c3823386a4cb7b611
saarthack <ssarthak2003@outlook.com>
2024-12-12 15:06:38 +0530

    message

--- class4/src/App.jsx
+++ class4/src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+
+    console.log('Title -',title);
+    console.log('Description -',desc);
+
+    setTitle('')
+    setDesc('')
+  }
+
   return (
     <div>
-      
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input 
+        value={title}
+        required
+        onChange={(e)=>{
+          setTitle(e.target.value)
+        }}
+        placeholder='Enter Title' 
+        type="text" />
+
+        <input 
+        value={desc}
+        required
+        onChange={(e)=>{
+        setDesc(e.target.value)
+        }}
+        placeholder='Enter Description' 
+        type="text" 
+        />
+        <button>Create</button>
+      </form>
     </div>
   )
 }
